6d6efb792ef14f4226939132954602e71bf7e09c
Kunihiro Kuroda <kunihiro.kuroda@gmail.com>
2023-07-17 23:25:15 +0900

    add prompt

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -5,12 +5,18 @@ const config = new Configuration({
   apiKey: process.env.OPENAI_API_KEY
 })
 const openai = new OpenAIApi(config)
- 
+const prompt = `あなたは金融のプロです。以下の条件に従って答えてください。子供にも分かるように答えてください。語尾は「〜〜だよ」「〜〜なんだ」など子供っぽい口調で答えてください。読みやすいように改行を入れて答えてください。銀行や金融、お金に関する話題のみ回答してください。それ以外は「ごめんね。僕にはむずかしくてわからないや。お金に関する質問なら答えられるよ。」と答えてください。`
+
 export const runtime = 'edge'
  
 export async function POST(req: Request) {
   const { messages } = await req.json()
- 
+  if(messages[0].role !== 'system'){
+    messages.unshift(
+      { role: 'system', content: prompt }
+    )
+  }
+
   const response = await openai.createChatCompletion({
     model: 'gpt-3.5-turbo',
     stream: true,
